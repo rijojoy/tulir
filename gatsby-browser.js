856ebf3a6a1de0287307099a114ee5c753244ac1
rijojoy-axatech <rijo.joy@axa-tech.com>
2019-08-08 16:02:23 +0530

    Fixed mobile menu

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
 import "./src/styles/bootstrap.min.css"
 import "./src/styles/main.css"
+import "./src/styles/responsive.css"
+import "./src/styles/animate.min.css"
--- src/components/Header/Nav/Nav.js
+++ src/components/Header/Nav/Nav.js
@@ -1,57 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "gatsby";
-const Nav = () => {
+class Nav extends Component {
 
-	const items = [
-                     { id:1, page:"/", title:"Home" },
-                     { id:2, page:"About", title:"About" },
-                     { id:3, page:"Blog", title:"Blog" },
-                     { id:4, page:"Price", title:"Menu" },
-                     { id:5, page:"News", title:"News" },
-                     { id:6, page:"Contact", title:"Contact us" }
-	              ];
-	let ItemLis = Object.keys(items).map( (idx) => {
-                      
-                      return <li><Link to={items[idx].page}>{items[idx].title}</Link></li>;
+	state = {
+		       isNavOpened:0
+	}
 
-	}) ;            
-	let navItems =    	
-					<nav id="navigation">
-				        <div class="container">
-				            <div class="row">
-				                <div class="col-md-12">
-				                    <div class="block">
-				                        <nav class="navbar navbar-default">
-				                          <div class="container-fluid">
-				                           {/* Brand and toggle get grouped for better mobile display */}
-				                            <div class="navbar-header">
-				                              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-				                                <span class="sr-only">Toggle navigation</span>
-				                                <span class="icon-bar"></span>
-				                                <span class="icon-bar"></span>
-				                                <span class="icon-bar"></span>
-				                              </button>
-				                                  <a class="navbar-brand" href="#">
-				                                    <img src="images/logo.png" alt="Logo" />
-				                                  </a>
+	onToggleNav = () => {
 
-				                            </div>
+           const cpyState = {...this.state};
+                 cpyState.isNavOpened = (!this.state.isNavOpened)?1:0;
+           this.setState(cpyState);
+	}
+	render(){
+        
+	    const items = [
+	                     { id:1, page:"/", title:"Home" },
+	                     { id:2, page:"About", title:"About" },
+	                     { id:3, page:"Blog", title:"Blog" },
+	                     { id:4, page:"Price", title:"Menu" },
+	                     { id:5, page:"News", title:"News" },
+	                     { id:6, page:"Contact", title:"Contact us" }
+		              ];
+		let ItemLis = Object.keys(items).map( (idx) => {
+	                      
+	                      return <li><Link to={items[idx].page}>{items[idx].title}</Link></li>;
 
-				                           {/* Collect the nav links, forms, and other content for toggling */}
-				                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-				                              <ul class="nav navbar-nav navbar-right" id="top-nav">
-				                                {ItemLis}
-				                              </ul>
-				                            </div>
-				                          </div>
-				                        </nav>
-				                    </div>
-				                </div>
-				            </div>
-				        </div>
-					</nav>
-					;
-	return  navItems;
+		}) ;  
+
+		let navBarClass = (!this.state.isNavOpened)?"navbar-toggle collapsed":"navbar-toggle";  
+		let collapseInClass = (!this.state.isNavOpened)?"collapse navbar-collapse":"collapse navbar-collapse in";         
+		let navItems =    	
+						<nav id="navigation">
+					        <div class="container">
+					            <div class="row">
+					                <div class="col-md-12">
+					                    <div class="block">
+					                        <nav class="navbar navbar-default">
+					                          <div class="container-fluid">
+					                           {/* Brand and toggle get grouped for better mobile display */}
+					                            <div class="navbar-header">
+					                              <button type="button" class={navBarClass} onClick = {this.onToggleNav} data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					                                <span class="sr-only">Toggle navigation</span>
+					                                <span class="icon-bar"></span>
+					                                <span class="icon-bar"></span>
+					                                <span class="icon-bar"></span>
+					                              </button>
+					                                  <a class="navbar-brand" href="#">
+					                                    <img src="images/logo.png" alt="Logo" />
+					                                  </a>
+
+					                            </div>
+
+					                           {/* Collect the nav links, forms, and other content for toggling */}
+					                            <div class={collapseInClass} id="bs-example-navbar-collapse-1">
+					                              <ul class="nav navbar-nav navbar-right" id="top-nav">
+					                                {ItemLis}
+					                              </ul>
+					                            </div>
+					                          </div>
+					                        </nav>
+					                    </div>
+					                </div>
+					            </div>
+					        </div>
+						</nav>
+						;
+
+						return  navItems;
+
+	}
+	
+	
 }
 
 export default Nav;
